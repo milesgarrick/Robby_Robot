d6c6199fca78d598b161b3dd4079730627b0477e
Miles Johnson <mgj2@cecs.pdx.edu>
2022-05-23 17:35:20 -0700

    Got state calculation working

--- main.py
+++ main.py
@@ -21,7 +21,53 @@ def print_grid(grid):
         print(i)
 
 
+def build_state(curr_y, curr_x, grid):
+    state = [0, 0, 0, 0, 0]
+    state[4] = grid[curr_y][curr_x]
+    # If at y extreme
+    if curr_y == 0 or curr_y == 9:
+        # Setting walls for y extremes
+        if curr_y == 0:
+            state[0] = 2
+            state[2] = grid[curr_y + 1][curr_x]
+        elif curr_y == 9:
+            state[0] = grid[curr_y - 1][curr_x]
+            state[2] = 2
+
+        # Setting walls for x extremes
+        if curr_x == 0:
+            state[3] = 2
+            # Square to the right is 1 or 0
+            state[1] = grid[curr_y][curr_x + 1]
+        elif curr_x == 9:
+            state[1] = 2
+            # Square to the left is 1 or 0
+            state[3] = grid[curr_y][curr_x - 1]
+        # Setting 1 or 0 for squares to the left and right
+        else:
+            state[1] = grid[curr_y][curr_x + 1]
+            state[3] = grid[curr_y][curr_x - 1]
+    # Not at y extreme
+    else:
+        if curr_x == 0:
+            state[1] = grid[curr_y][curr_x + 1]
+            state[3] = 2
+        elif curr_x == 9:
+            state[1] = 2
+            state[3] = grid[curr_y][curr_x - 1]
+        else:
+            state[1] = grid[curr_y][curr_x + 1]
+            state[3] = grid[curr_y][curr_x - 1]
+        state[0] = grid[curr_y - 1][curr_x]
+        state[2] = grid[curr_y + 1][curr_x]
+    return state
+
+
 def learn(q_matrix, grid):
+    curr_y, curr_x = (random.randint(0, 9), random.randint(0, 9))
+    print("Starting location: (%d, %d)" % (curr_y, curr_x))
+    curr_state = build_state(curr_y, curr_x, grid)
+    print("First state: ", curr_state)
     return
 
 
