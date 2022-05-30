6600ff7b28aa23b3f558bab2c872bde4f34bafd5
Miles Johnson <mgj2@cecs.pdx.edu>
2022-05-30 16:59:45 -0700

    Fixed reward values

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import random
+import statistics as st
 
 
 def build_grid():
     grid = []
-    ratio = random.randint(1, 3)
+    # ratio = random.randint(1, 3)
+    ratio = 5
     for i in range(10):
         cols = []
         for j in range(10):
@@ -64,17 +66,16 @@ def build_state(curr_y, curr_x, grid):
     return state
 
 
-def pick_action(q_matrix, curr_state):
-    epsilon = 0.1
-    x = random.random(0, 1)
+def pick_action(q_matrix, curr_state, epsilon):
+    x = random.random()
+    action = str(random.randint(0, 4))
     if x < epsilon:
         # pick randomly
         return str(random.randint(0, 4))
     else:
         # pick greedily
-        action = '0'
         max_reward = q_matrix.get(curr_state + action)
-        for i in range(1, 5):
+        for i in range(5):
             i = str(i)
             if q_matrix.get(curr_state + i) > max_reward:
                 max_reward = q_matrix.get(curr_state + i)
@@ -82,68 +83,76 @@ def pick_action(q_matrix, curr_state):
         return action
 
 
-def get_reward(grid, curr_y, curr_x, curr_state, action) -> int:
+def get_reward(grid, curr_y, curr_x, curr_state, action) -> tuple[int, str, int, int]:
     if action == '4':
         if grid[curr_y][curr_x] == 1:
-            return 1
+            grid[curr_y][curr_x] = 0
+            return 10, curr_state, curr_y, curr_x
         else:
-            return 0, curr_state
+            return -1, curr_state, curr_y, curr_x
     else:
-        if action == '0':
-            x = curr_x
+        if curr_state[int(action)] == '2':
+            return -5, curr_state, curr_y, curr_x
+        elif action == '0':
             y = curr_y - 1
+            x = curr_x
         elif action == '1':
             x = curr_x + 1
             y = curr_y
         elif action == '2':
             x = curr_x
             y = curr_y + 1
-        else:
+        elif action == '3':
             x = curr_x - 1
             y = curr_y
-        if grid[y][x] == 2:
-            return -1, curr_state
-        else:
-            new_state = build_state(y, x, grid)
-            return 0, new_state
+        new_state = build_state(y, x, grid)
+        return 0, new_state, y, x
 
 
 def get_q_value(q_matrix, curr_state, action, reward, next_state):
     q = q_matrix.get(curr_state + action)
-    q_prime = max(q_matrix.get(next_state + '0'),
-                  q_matrix.get(next_state + '1'),
-                  q_matrix.get(next_state + '2'),
-                  q_matrix.get(next_state + '3'),
-                  q_matrix.get(next_state + '4'))
+    q_prime = max(int(q_matrix.get(next_state + '0')),
+                  int(q_matrix.get(next_state + '1')),
+                  int(q_matrix.get(next_state + '2')),
+                  int(q_matrix.get(next_state + '3')),
+                  int(q_matrix.get(next_state + '4')))
     eta = 0.2
     gamma = 0.9
     q_value = q + (eta * (reward + (gamma * q_prime) - q))
     return q_value
 
 
-def learn(q_matrix, grid):
+def learn(q_matrix, grid, epsilon_flag):
+    data_set = []
     total_reward = 0
 
     # Robby's current coordinates
     curr_y, curr_x = (random.randint(0, 9), random.randint(0, 9))
-    print("Starting location: (%d, %d)" % (curr_y, curr_x))
 
     # Encoding the location state
     curr_state = build_state(curr_y, curr_x, grid)
+    # 5 possible actions per state.
+    # 0 = up
+    # 1 = right
+    # 2 = down
+    # 3 = left
+    # 4 = pick up can
+    for i in range(5):
+        # Adds the state-action encoding to the Q Matrix.  If state-action pair already exists, does nothing.
+        q_matrix.setdefault(curr_state + str(i), 0)
+
+    epsilon = 0.1
 
     for m in range(200):
-        # 5 possible actions per state.
-        # 0 = up
-        # 1 = right
-        # 2 = down
-        # 3 = left
-        # 4 = pick up can
-        for i in range(5):
-            # Adds the state-action encoding to the Q Matrix.  If state-action pair already exists, does nothing.
-            q_matrix.setdefault(curr_state + str(i), 0)
+        # reduce epsilon ever 50 epochs
+        if epsilon_flag:
+            if (m + 1) % 50 == 0:
+                epsilon -= 0.025
 
-        action = pick_action(q_matrix, curr_state)
-        reward, next_state = get_reward(grid, curr_y, curr_x, action)
+        action = pick_action(q_matrix, curr_state, epsilon)
+        reward, next_state, curr_y, curr_x = get_reward(grid, curr_y, curr_x, curr_state, action)
+        for i in range(5):
+            q_matrix.setdefault(next_state + str(i), 0)
         q_value = get_q_value(q_matrix, curr_state, action, reward, next_state)
         q_matrix.update({(curr_state + action): q_value})
         curr_state = next_state
@@ -152,21 +161,38 @@ def learn(q_matrix, grid):
 
 
 def main():
-    print("Main")
+    f = open("data.txt", 'w')
     q_matrix = dict()
     n = 5000
     total_reward = 0
+    episode_reward = 0
+    data_set = []
+    for i in range(n):
+        # print()
+        # print("Episode ", i)
+        grid = build_grid()
+        episode_reward = learn(q_matrix, grid, True)
+        total_reward += episode_reward
+        # print("Episode reward: ", episode_reward)
+        print(episode_reward)
+        f.write(str(episode_reward) + "\n")
+    avg_reward = total_reward / n
+    total_reward = 0
+    print("Learning avg reward: ", avg_reward)
+    print("==================================")
+    f.write("Learning avg reward: " + str(avg_reward) + "\n")
+
     for i in range(n):
         grid = build_grid()
-        print_grid(grid)
-        total_reward += learn(q_matrix, grid)
-    avg_reward = total_reward / n
-    print("Learning avg reward: ", avg_reward)
+        episode_reward = learn(q_matrix, grid, False)
+        data_set.append(episode_reward)
+        total_reward += episode_reward
 
-    grid = build_grid()
-    test_reward = learn(q_matrix, grid)
-    print("Test reward: ", test_reward)
-
+    print("Testing standard deviation: ", st.stdev(data_set))
+    print("Average testing reward: ", total_reward / n)
+    f.write("Testing standard deviation: " + str(st.stdev(data_set)) + "\n")
+    f.write("Average testing reward: " + str(total_reward / n))
+    f.close()
 
 
 if __name__ == '__main__':
